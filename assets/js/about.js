a8c284a01863f051d9eb75f8c63559fb6163af70
Aman Jain <87036083+Amantechchampion@users.noreply.github.com>
2023-05-05 17:18:30 +0530

    initial commit

---
+++ assets/js/about.js
@@ -0,0 +1,63 @@
+/*About Details Table*/
+
+const aboutTable = document.querySelector(".main");
+
+
+
+AOS.init();
+const fillData = () => {
+  let output = "";
+  about.forEach(
+    ({
+      image,
+      title,
+      authors,
+      conferences,
+      aboutYr,
+      citebox,
+      citation,
+      absbox,
+      abstract,
+    }) =>
+      (output += `
+            <tr data-aos="zoom-in-left"> 
+                <td class="imgCol"><img src="${image}" class="rImg"></td>
+                <td class = "aboutTitleName">
+                    <div class="img-div">
+                        <span class="imgResponsive">
+                            <img src="${image}" class="imgRes">
+                        </span>
+                    </div>
+                    <a href="#0" class="paperTitle"> ${title} </a> 
+                    <div class = "authors"> ${authors} </div> 
+                    
+                    <div class="rConferences"> ${conferences} 
+                        <div class="aboutY">${aboutYr}</div>
+                    </div>
+                    
+                    <!--CITE BUTTON-->
+                    <div class="d-flex" style="margin-right:5%;">
+                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
+                            ABSTRACT
+                        </button>
+                
+                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
+                            CITE
+                        </button>
+                    </div>
+                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
+                        <div class="card-body">
+                            ${abstract}    
+                        </div>
+                    </div>
+                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
+                        <div class="card-body">
+                            ${citation.vancouver}    
+                        </div>
+                    </div>
+                </td>
+            </tr>`)
+  );
+  aboutTable.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", fillData);
