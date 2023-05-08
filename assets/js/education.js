c2352b9cb4b574e654871bd38591d1647cbc2aee
Aman Jain <87036083+Amantechchampion@users.noreply.github.com>
2023-05-09 01:25:00 +0530

    remove and updated

--- assets/js/education.js
+++ assets/js/education.js
@@ -4,67 +4,37 @@ AOS.init();
 
 const moocs = document.querySelector(".moocs");
 const moocscards = [
+  
   {
-    title: "Data Science",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/browse/data-science",
-  },
-  {
-    title: "Cryptography",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
-  },
-  {
-    title: "Machine Learning",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/machine-learning",
-  },
-  {
-    title: "Introduction to HTML 5",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
-  },
-  {
-    title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
-  },
-  {
-    title: "Javascript",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
-  },
-  {
-    title: "Bootstrap 4",
+    title: "Web Development",
     cardImage: "assets/images/education-page/udemy.svg",
     moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
   },
   {
-    title: "Intro to React",
+    title: "Python-Django",
+    cardImage: "assets/images/education-page/udacity.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
+  },
+  {
+    title: "Google Cloud Fundamentals",
+    cardImage: "assets/images/education-page/coursera2.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
+  },
+  {
+    title: "MEARN Stack",
     cardImage: "assets/images/education-page/edx.svg",
     moocLink: "https://www.edx.org/learn/reactjs",
   },
   {
-    title: "Intro to React Native",
+    title: "Google Cloud Computing Foundation",
+    cardImage: "assets/images/jecrc-foundation-logo-1jecrc-foundation-logo",
+    moocLink: 
+      "",
+  },
+  {
+    title: "Essential Google Cloud Infrastucture",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink:
-      "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
-  },
-  {
-    title: "NodeJS, Express and MongoDB",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/server-side-nodejs",
-  },
-  {
-    title: "XML-AJAX",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
-  },
-  {
-    title: "Data Structures & Algorithms",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink:
-      "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
+    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
   },
 ];
 
